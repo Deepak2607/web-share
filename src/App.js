f6e22dc575fe170eb50a47e58eecfaae888ae650
Deepak260797 <deepak.kumrawat@okcredit.in>
2020-05-02 01:23:36 +0530

    last commits

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import image from "./workhour1.jpg";
 class App extends Component {
 
        handleOnClick=() => {
-            // window.open(
-            //     `https://api.whatsapp.com/send?text=Share%20on%20Whatsapp%20http%3A%2F%2Fhttps://okcredit.in%20`,
-            //     "_blank"
-            // )
-            // const files = [image];
-            if (navigator.canShare && navigator.canShare({ })){
-                navigator.share({
-                        title: 'Vacation Pictures',
-                        text: 'Photos from September 27 to October 14.',
-                        // file: files,
-                    })
-                    .then(() => console.log('Successful share'))
-                    .catch((error) => console.log('Error sharing', error));
-            } else {
-                console.log("Web Share API is not supported in your browser.")
+
+            if (navigator.share) {
+              navigator.share({
+                title: 'web.dev',
+                text: 'Check out web.dev.',
+                url: 'https://okcredit.in',
+              })
+                .then(() => console.log('Successful share'))
+                .catch((error) => console.log('Error sharing', error));
+            }else {
+              console.log(`Your system doesn't support sharing files.`);
             }
         }
 
@@ -29,22 +25,8 @@ class App extends Component {
             } 
           }
         }
-
-
-
  
     render () {
-              
-            // const onLoad()=> {
-            //   if (navigator.share === undefined) {
-            //     if (window.location.protocol === 'http:') {
-            //       window.location.replace(window.location.href.replace(/^http:/, 'https:'));
-            //     } 
-            //   }
-            // }
-
-            // window.addEventListener('load', onLoad);
-        
         return (
             <div> 
             <div className="container-fluid">
