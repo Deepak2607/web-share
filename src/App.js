06a87211b10ee40ab98dc56fb6ec2311ae8641fc
Deepak2607 <deepakkumrawat8@gmail.com>
2020-05-16 16:15:24 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import {Button} from 'react-materialize';
-import image from "./share.jpg";
-class App extends Component {
+import React, { useState, useEffect } from 'react';
+import {Button, TextInput, Card} from 'react-materialize';
+const App = () =>{
 
+       const [title, setTitle] = useState();
+       const [text, setText] = useState();
+       const [url, setUrl]= useState();
+       const [file, setFile] = useState();
 
-       async handleOnClick(){
+       const handleTitle= (event)=>{
+        setTitle(event.target.value);
+       }
+       const handleText= (event)=>{
+        setText(event.target.value);
+       }
+       const handleUrl= (event)=>{
+        setUrl(event.target.value);
+       }
+       const handleFile= (event)=>{
+        setFile(event.target.files[0]);
+       }
 
-            const response = await fetch(image);
-            const blob = await response.blob();
-            const file = new File([blob], 'share.jpg', {type: blob.type});
-            console.log(file);
-        
+       async function handleOnSubmit(){
+
+            // const response = await fetch(image);
+            // const blob = await response.blob();
+            // const file = new File([blob], 'share.jpg', {type: blob.type});
+            // console.log(file);
+
             if(navigator.share) {
               await navigator.share({
-                title: 'Share',
-                text: 'Download the app and buy essential items from your nearby shops.',
-                url: 'https://okcredit.in',
+                title: title,
+                text: text,
+                url: url,
                 files: [file]     
               })
                 .then(() => console.log('Successful share'))
@@ -23,59 +39,57 @@ class App extends Component {
             }else {
               console.log(`system does not support sharing files.`);
             }
-
-            // var file = new File(['okcredit'], './okcredit.jpeg', {type:'image/jpg'});
-            // console.log(file);
-            // let filesArray = [];
-            // filesArray.push(file);
-
-            // if (navigator.canShare && navigator.canShare({ files: filesArray })) {
-            //     await navigator.share({
-            //             title: 'Share',
-            //             text: 'Download the app and buy essential items from your nearby shops.',
-            //             url: 'https://okcredit.in',
-            //             file: files
-            //         })
-            //         .then(() => console.log('Successful share'))
-            //         .catch((error) => console.log('Error sharing', error));
-            // }else{
-            //     console.log("system doesn't support sharing files");
-            // }
-            
+       
         }
          
-        componentDidMount= ()=> {
+        useEffect(()=> {
           if (navigator.share === undefined) {
             if (window.location.protocol === 'http:') {
               window.location.replace(window.location.href.replace(/^http:/, 'https:'));
             } 
           }
+        }, []);
+
+   
+        // let style1={
+        // display: "flex",
+        // alignItems: "center",
+        // justifyContent: "center",
+        // flexDirection: "column",
+        // minHeight: "100vh",
+        // }
+
+        // let style2= { 
+        // marginBottom: "10px",
+        // textAlign: "center",
+        // marginRight: 30,
+        // marginLeft: 30,
+        // }
+
+        let style3= {
+          color:"lightseagreen"
         }
 
-    render () {
-
-    let style1={
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column",
-            minHeight: "100vh",
-            }
-
-    let style2= { 
-            marginBottom: "10px",
-            textAlign: "center",
-            marginRight: 30,
-            marginLeft: 30,
-            }
-
         return (
-            <div style={style1}>
-            <div style={style2} className="container-fluid">
-            <Button node="a" style={{ color:"white"}} waves="light" onClick={this.handleOnClick}>Shar</Button>
-            </div></div>
+            <div>
+            <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-4"></div>
+              <div className="col-md-4">
+              <Card>
+              <h4 style={style3}>Share on any social platform</h4>
+              <TextInput value={title} onChange={handleTitle} label="Title" />
+              <TextInput value={text} onChange={handleText} label="Text" />
+              <TextInput value={url} onChange={handleUrl} label="Url" />
+              <TextInput onChange={handleFile} label="File" type="file" />
+              <Button node="a" style={{ color:"white"}} waves="light" onClick={handleOnSubmit} >Share</Button>
+              </Card>
+              </div>
+              <div className="col-md-4"></div>
+            </div>   
+            </div>
+            </div>
     );
-  }  
 }
 
 export default App;
